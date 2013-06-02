579100baa74ed5ad73ad69f5f015b20f6212df8c
scheuermann <scheuermann@constantins-macbook-air.fritz.box>
2013-06-02 18:38:58 +0200

    [COS] BootStrap successfully added and applied to ViewHelper - createMenuBar 	bootstrapped - htmlbegin + end 	bootstrapped

--- src/com/kupkik/ui/html/view/ViewHelper.java
+++ src/com/kupkik/ui/html/view/ViewHelper.java
@@ -12,138 +12,145 @@ import com.kupkik.ui.html.HtmlRequestProcessor;
  */
 public class ViewHelper
 {
-    private HttpSession mSession;
+	private HttpSession mSession;
 
-    public ViewHelper(final HttpSession pSession)
-    {
-        mSession = pSession;
-    }
+	public ViewHelper(final HttpSession pSession)
+	{
+		mSession = pSession;
+	}
 
-    /**
-     * Converts text, so that it can be displayd in a html-site.
-     * 
-     * @param pText
-     *            the text which should be shown inside a html-site
-     * @return a text converted for HTML
-     */
-    public String convertTextForHtml( final String pText )
-    {
-        return StringEscapeUtils.escapeHtml(pText);
-    }
+	/**
+	 * Converts text, so that it can be displayd in a html-site.
+	 * 
+	 * @param pText
+	 *            the text which should be shown inside a html-site
+	 * @return a text converted for HTML
+	 */
+	public String convertTextForHtml( final String pText )
+	{
+		return StringEscapeUtils.escapeHtml(pText);
+	}
 
-    /**
-     * Creates the menu-bar on top of the view.
-     * 
-     * @param pHtmlContent
-     *            The content of the HTML-site, where the menu-bar should be
-     *            added to.
-     */
-    private void createMenuBar( final StringBuilder pHtmlContent )
-    {
-        pHtmlContent.append("   <table border=\"0\" width=\"100%\">\n");
-        pHtmlContent.append("       <colgroup>\n");
-        pHtmlContent.append("           <col width=\"1*\">\n");
-        pHtmlContent.append("           <col width=\"1*\">\n");
-        pHtmlContent.append("           <col width=\"1*\">\n");
-        pHtmlContent.append("       </colgroup>\n");
-        pHtmlContent.append("       <tr>\n");
-        pHtmlContent.append("           <td>\n");
+	/**
+	 * Creates the menu-bar on top of the view.
+	 * 
+	 * @param pHtmlContent
+	 *            The content of the HTML-site, where the menu-bar should be
+	 *            added to.
+	 */
+	private void createMenuBar( final StringBuilder pHtmlContent )
+	{
 
-        UserWithPassword currentUser = (UserWithPassword) mSession.getAttribute("currentUser");
-        if( currentUser.getPasswordMd5().equals("none") )
+		pHtmlContent.append(" <div class=\"navbar\">");
+		pHtmlContent.append("  <div class=\"navbar-inner\">");
+		pHtmlContent.append("    <a class=\"brand\" href=\"#\">KUPKIK - ");
+
+		UserWithPassword currentUser = (UserWithPassword) mSession.getAttribute("currentUser");
+		pHtmlContent.append(currentUser.getName());
+		
+		pHtmlContent.append(" </a>");
+		pHtmlContent.append(" 		<ul class=\"nav\">");
+		pHtmlContent.append(" 	      <li class=\"active\"><a href=\"#\">Home</a></li>");
+		
+
+						/**if( currentUser.getPasswordMd5().equals("none") )
         {
             pHtmlContent.append("<i>");
         }
-        pHtmlContent.append(currentUser.getName());
         if( currentUser.getPasswordMd5().equals("none") )
         {
             pHtmlContent.append("</i>");
         }
+						 */
 
-        pHtmlContent.append("           </td>\n");
-        pHtmlContent.append("           <td>\n");
+		if( currentUser.getPasswordMd5().equals(HtmlRequestProcessor.GUEST_USER.getPasswordMd5()) )
+		{
+			pHtmlContent.append(" <li><a href=\"/?showView=LoginView\">Login</a> </li>");
+		}
+		else
+		{
+			pHtmlContent.append(" <li><a href=\"/?action=Logoff\">Logoff</a> </li>");
+		}
 
-        if( currentUser.getPasswordMd5().equals(HtmlRequestProcessor.GUEST_USER.getPasswordMd5()) )
-        {
-            pHtmlContent.append("<a href=\"/?showView=LoginView\">Login</a>");
-        }
-        else
-        {
-            pHtmlContent.append("<a href=\"/?action=Logoff\">Logoff</a>");
-        }
 
-        pHtmlContent.append("           </td>\n");
-        pHtmlContent.append("           <td>\n");
-        pHtmlContent.append("           </td>\n");
-        pHtmlContent.append("       </tr>\n");
-        pHtmlContent.append("   </table>\n");
-    }
+		pHtmlContent.append(" </ul>");
+		pHtmlContent.append(" </div>");
+		pHtmlContent.append(" </div>");
 
-    /**
-     * @see #createHtmlBegin(boolean)
-     */
-    public String createHtmlBegin()
-    {
-        return createHtmlBegin("");
-    }
+	}
 
-    /**
-     * @see #createHtmlBegin(String, boolean)
-     */
-    public String createHtmlBegin( final String pTitle )
-    {
-        return createHtmlBegin(pTitle, true);
-    }
+	/**
+	 * @see #createHtmlBegin(boolean)
+	 */
+	public String createHtmlBegin()
+	{
+		return createHtmlBegin("");
+	}
 
-    /**
-     * Creates the beginning of a HTML-site.
-     * 
-     * @param pTitle
-     *            the title of the page
-     * @param pDoShowMenubar
-     *            Should the menu-bar be shown?
-     * @return The content of a HTML-site which contains the beginning of this
-     *         site.
-     */
-    public String createHtmlBegin( final String pTitle, final boolean pDoShowMenubar )
-    {
-        final StringBuilder htmlBegin = new StringBuilder();
-        htmlBegin.append("<html>\n");
-        htmlBegin.append("   <head>\n");
+	/**
+	 * @see #createHtmlBegin(String, boolean)
+	 */
+	public String createHtmlBegin( final String pTitle )
+	{
+		return createHtmlBegin(pTitle, true);
+	}
 
-        if( pTitle.equals("") )
-        {
-            htmlBegin.append("      <title>kupkik</title>\n");
-        }
-        else
-        {
-            htmlBegin.append("      <title>kupkik | " + pTitle + "</title>\n");
-        }
+	/**
+	 * Creates the beginning of a HTML-site.
+	 * 
+	 * @param pTitle
+	 *            the title of the page
+	 * @param pDoShowMenubar
+	 *            Should the menu-bar be shown?
+	 * @return The content of a HTML-site which contains the beginning of this
+	 *         site.
+	 */
+	public String createHtmlBegin( final String pTitle, final boolean pDoShowMenubar )
+	{
+		final StringBuilder htmlBegin = new StringBuilder();
+		htmlBegin.append("<html>\n");
+		htmlBegin.append("   <head>\n");
 
-        htmlBegin.append("      <meta name=\"robots\" content=\"noindex\">\n");
-        htmlBegin.append("   </head>\n");
-        htmlBegin.append("  <body>\n");
+		if( pTitle.equals("") )
+		{
+			htmlBegin.append("      <title>kupkik</title>\n");
+		}
+		else
+		{
+			htmlBegin.append("      <title>kupkik | " + pTitle + "</title>\n");
+		}
 
-        if( pDoShowMenubar )
-        {
-            createMenuBar(htmlBegin);
-        }
+		htmlBegin.append("      <meta name=\"robots\" content=\"noindex\">\n");
+		//BootStrap Include
+		htmlBegin.append("		<link href=\"/res/bootstrap/css/bootstrap.min.css\" rel=\"stylesheet\" media=\"screen\">");
+		htmlBegin.append("   </head>\n");
+		htmlBegin.append("  <body>\n");
+		htmlBegin.append("		<script src=\"http://code.jquery.com/jquery.js\"></script>");
+		htmlBegin.append("		<script src=\"js/bootstrap.min.js\"></script>");
+		//Open the Bootstrap Container
+		htmlBegin.append("		<div class=\"container\">");
 
-        return htmlBegin.toString();
-    }
+		if( pDoShowMenubar )
+		{
+			createMenuBar(htmlBegin);
+		}
 
-    /**
-     * Creates the end of a HTML-site.
-     * 
-     * @return The content of a HTML-site which contains the end of this site.
-     */
-    public String createHtmlEnd()
-    {
-        final StringBuilder htmlEnd = new StringBuilder();
+		return htmlBegin.toString();
+	}
 
-        htmlEnd.append("  </body>\n");
-        htmlEnd.append("</html>\n");
+	/**
+	 * Creates the end of a HTML-site.
+	 * 
+	 * @return The content of a HTML-site which contains the end of this site.
+	 */
+	public String createHtmlEnd()
+	{
+		final StringBuilder htmlEnd = new StringBuilder();
+		//Bootstrap Container Closed
+		htmlEnd.append("  </div>\n");
+		htmlEnd.append("  </body>\n");
+		htmlEnd.append("</html>\n");
 
-        return htmlEnd.toString();
-    }
+		return htmlEnd.toString();
+	}
 }
